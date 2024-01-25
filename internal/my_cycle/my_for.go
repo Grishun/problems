4debd4f525e287e278d075948212c1d4b207918e
Grishu <grishudia@proton.me>
2024-01-25 22:31:21 +0300

    created my_for

--- internal/my_cycle/my_for.go
+++ internal/my_cycle/my_for.go
@@ -1,1 +1,10 @@
 package my_cycle
+
+func MyFor(n, k int, fn func()) {
+	if n == k+1 {
+		return
+	}
+	fn()
+	n++
+	MyFor(n, k, fn)
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"problems/internal/my_cycle"
+)
+
+func main() {
+	my_cycle.MyFor(0, 1, func() {
+		fmt.Println("jopa")
+	})
+}
