6393f97f688c04ec18bea67693c427d344f3828c
Grishu <grishudia@proton.me>
2024-03-21 17:01:35 +0300

    rewrote documentation for DivStr()

--- task/divide_string/pkg/divide_string/divide_string.go
+++ task/divide_string/pkg/divide_string/divide_string.go
@@ -1,6 +1,10 @@
 package divide_string
 
-// DivStr is a function, that divides string by 2 equaled by len parts
+// DivStr is a function, that divides string to even numbers of substrings.
+// If string has odd number of symbols, function adds `_` to the end of string.
+//
+// Example:
+// "123" -> ["12", "3_"]
 func DivStr(str string) (res []string) {
 	if len(str)%2 != 0 {
 		str += "_"
