844804f1b3e6841f1ba04d124b804d0f17afa460
Grigoriy Reuk <grishu@Grigoriys-MacBook-Air.local>
2024-06-19 19:16:21 +0300

    added PerfectNums()

--- task/math/pkg/numbers/numbers.go
+++ task/math/pkg/numbers/numbers.go
@@ -189,20 +189,3 @@ func DecompNums(num int) (divs []int) {
 	return
 
 }
-
-// PerfectNums returns all great numbers less than n
-func PerfectNums(n int) (res []int) {
-
-	for i := 6; i < n; i++ {
-		sumOfDivs := 0
-		for j := 0; j < len(DecompNums(i))-1; j++ {
-			sumOfDivs += DecompNums(i)[j]
-		}
-
-		if sumOfDivs == i {
-			res = append(res, i)
-		}
-	}
-
-	return
-}
